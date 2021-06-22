e5448174bdbc29d86b6d9f78ca620431aee8acd1
diego40g <diegopaznaula@hotmail.com>
2021-06-22 15:10:24 -0500

    guitar project

---
+++ PazDiego_Guitarras/app/src/main/java/com/dipaz/pazdiego_guitarras/AdminSQLOpenHelper.java
@@ -0,0 +1,23 @@
+package com.dipaz.pazdiego_guitarras;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class AdminSQLOpenHelper extends SQLiteOpenHelper {
+
+    public AdminSQLOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table articulos(codigo int primary key,descripcion text,precio real)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
